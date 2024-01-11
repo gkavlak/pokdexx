8394e2533ae7b1954e825f99b3b21fbf89773a72
gkavlak <63876977+gkavlak@users.noreply.github.com>
2024-01-11 13:29:09 +0300

    first

--- pokedexx/App.js
+++ pokedexx/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>asdyour app!</Text>
+      <Text>gokhan kavlak app!</Text>
       <StatusBar style="auto" />
     </View>
   );
